a21e580edd9bda0b2db24b79edfafa0d4d1d7d7d
Spencer Fisher <Fisher.spencer@gmail.com>
2026-06-30 15:12:15 -0700

    Add: generate-parentheses - submission-3

---
+++ Data Structures & Algorithms/generate-parentheses/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        combinations = []
+        length = 2**(2*n)
+        #print(f"length: {length}")
+        for i in range(length):
+            c = ""
+            count = 0
+            for j in range(n*2):
+                b = 2 ** j
+                if b & i:
+                    count += 1
+                    c+="("
+                else:
+                    count -= 1
+                    c+=")"
+                    if count < 0:
+                        #print(f"{i}: invalid set:   {c}")
+                        break
+            if count == 0:
+                #print(f"{i}: valid set:   {c}")
+                combinations.append(c)
+            
+        return combinations
