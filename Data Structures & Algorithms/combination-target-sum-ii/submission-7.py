7f0f8b5a373cf77a8134eb5fa8d3509d58977fe7
Spencer Fisher <Fisher.spencer@gmail.com>
2026-07-02 18:42:08 -0700

    Add: combination-target-sum-ii - submission-7

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-7.py
@@ -0,0 +1,42 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        def getCombinationsRecursive(skipped: set[int], currentSet: List[int], candidates: List[int], target: int) -> bool:
+            # we solved!
+            if target == 0:
+                sum_sets.append(currentSet)
+                return
+
+            # remove values too large
+            while(len(candidates) > 0 and candidates[-1] > target):
+                candidates.pop()
+            
+            # is no solution here?
+            if len(candidates) == 0:
+                return
+
+            i = candidates.pop()
+            # if the number hasnt previously been skipped
+            # try getting the set with the current largest value
+            if i not in skipped:
+                newSet = currentSet[:]
+                newSet.append(i)
+                # dupe set to avoid muddying current skip set
+                newSkipped = set(skipped)
+                getCombinationsRecursive(newSkipped, newSet, candidates[:], target - i)
+            
+            newSet = currentSet[:]
+            # add the number to the skipped set
+            newSkipped = set(skipped)
+            newSkipped.add(i)
+            # try getting the set without the current largest value
+            getCombinationsRecursive(newSkipped, newSet, candidates[:], target)
+            return
+
+        # generate all subsets:
+        sum_sets = []
+        # sorting probably speeds up the solve?
+        candidates.sort()
+        length = len(candidates)
+        getCombinationsRecursive([],[], candidates, target)
+
+        return sum_sets
