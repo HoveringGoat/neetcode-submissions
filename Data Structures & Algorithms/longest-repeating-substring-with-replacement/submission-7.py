470532c19c4a0c4eb6614cca73b7c1e05b2d3e43
Spencer Fisher <Fisher.spencer@gmail.com>
2026-07-13 17:27:50 -0700

    Add: longest-repeating-substring-with-replacement - submission-7

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-7.py
@@ -0,0 +1,87 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        start = -2
+        end = -1
+        char = ''
+        left = 0
+        right = 0
+        maxSize = 0
+        currSize = 0
+
+        while start <= end and end + 1 < len(s) - 1:
+            # move substring up
+            start = end + 1
+
+            # get char for substring
+            char = s[start]
+
+            # find end of substring
+            for end in range(start, len(s)):
+                if end+1 < len(s) and s[end+1] == char:
+                    continue
+                break
+
+            # we have found the start and end of the next substring
+            left = start
+            right = end
+
+            # only do move logic if k > 0
+            if k == 0:
+                size = right - left + 1
+                maxSize = max(maxSize, size)
+                continue
+            
+            # move k places left
+            leftK = 0
+            while leftK <= k and left > 0:
+                left -= 1
+                if s[left] != char:
+                    leftK += 1
+
+                    # if we are over budget
+                    # move back and break
+                    if leftK > k:
+                        left += 1
+                        break
+
+            # record size at max left
+            size = right - left + 1
+            maxSize = max(maxSize, size)
+
+            # shift right k place
+            rightK = 0
+
+            # loop while budget isnt over allocated and right is in bounds
+            while rightK <= k and right < len(s)-1:
+                # always move right - check after if its valid
+                right += 1
+                if s[right] == char:
+                    continue
+
+                # uses right k budget
+                rightK += 1 
+                
+                # we're at budget
+                if rightK + leftK > k:
+                    # if k buget exhausted move left back to the right
+                    while leftK > 0 and rightK + leftK > k:
+                        left += 1
+                        if s[left-1] != char:
+                            leftK -= 1
+
+                    # we're either at budget now
+                    # or leftK is zero and rightK is over utilizated
+
+                    if rightK > k:
+                        # we've gone too far right.
+                        # move back and break
+                        right -= 1
+                        break
+                    size = right - left + 1
+                    maxSize = max(maxSize, size)
+
+            # check size after exiting loop
+            size = right - left + 1
+            maxSize = max(maxSize, size)
+
+        return maxSize
