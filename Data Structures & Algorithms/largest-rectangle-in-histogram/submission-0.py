dbcc8db8c3b9ddc920ade53253f77b0185552fa2
Spencer Fisher <Fisher.spencer@gmail.com>
2026-08-03 18:59:56 -0700

    Add: largest-rectangle-in-histogram - submission-0

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-0.py
@@ -0,0 +1,40 @@
+class Area:
+    height: int
+    start: int
+    def __init__(self, start: int, height: int):
+        self.start = start
+        self.height = height
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+
+        # largest area so far
+        maxArea: int = 0
+
+        # the largest areas using the previous column
+        previousAreas = []
+
+        for column, height in enumerate(heights):
+            newAreas = []
+            maxAreaHeight = 0
+            for area in previousAreas:
+                if area.height <= height: 
+                    newAreas.append(area)
+                    maxAreaHeight = area.height
+                    continue
+                break
+            
+            for newAreaHeight in range(maxAreaHeight, height+1):
+                newAreas.append(Area(column, newAreaHeight))
+            
+            for area in newAreas:
+                size = (column - area.start + 1) * area.height
+                maxArea = max(size, maxArea)
+            
+            previousAreas = newAreas
+
+        return maxArea
+
+            
+
+        
